96ec19bbe6e41ae2ce16324d1b3ead92025bd4fc
sammyrulez <sammyrulez@gmail.com>
2015-02-12 21:25:39 +0100

    removed formal sideeffect

--- chp1/first_index_of_any.py
+++ chp1/first_index_of_any.py
@@ -1,10 +1,9 @@
 from fn.iters import first
 
 def first_index_of(string,chars):
-    indexed = zip(range(0,len(string)),string)
 
     def lazy_index_generator():
-        for pair in indexed:
+        for pair in zip(range(0,len(string)),string):
             for c in chars:
                 if c == pair[1]:
                     yield pair[0]
